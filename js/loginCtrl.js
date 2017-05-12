82a4fc7479bc06174813d7ea3f01138dbba7bd6f
Nagaraj Chandran <nagaraj.chandran@in.ibm.com>
2017-05-12 17:38:01 +0530

    Sign In pages

--- js/loginCtrl.js
+++ js/loginCtrl.js
@@ -4,13 +4,24 @@
       'use strict';
     
 myApp.controller('loginCtrl', function($scope) {
+      $scope.signInScreen = true;
+      $scope.signUpScreen = false;
+      $scope.forgotPswdScreen = false;
+      $scope.pswdResetScreen = false; 
+      $scope.successSignUpScreen = false;
       $scope.pswdblankValidation = false;
       $scope.emailblankValidation=false;
 
     $scope.signInSubmit = function(){
-        $scope.emailblankValidation=true;
-        $scope.pswdblankValidation = true;
-       }
+      $scope.emailblankValidation=true;
+      $scope.pswdblankValidation = true;
+    }
+    $scope.signUp = function(){
+      $scope.signUpScreen = true;
+      $scope.signInScreen = false;
+      
+      alert($scope.signUpScreen);
+    }
 
      var myDate = new Date();
         var hrs = myDate.getHours();
