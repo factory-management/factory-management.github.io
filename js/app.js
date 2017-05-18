30d4ad56d8f7edfc21e0227bc230b7d60923e6d7
AshokNaik <ashonaik@in.ibm.com>
2017-05-18 12:45:59 +0530

    app.js uploaded

--- js/app.js
+++ js/app.js
@@ -19,57 +19,21 @@
         }
       });
     })
-
+    
     .config(function($stateProvider, $urlRouterProvider) {
       $stateProvider
       .state('login', {
         url: '/login',  
         templateUrl: 'templates/login.html',
-        controller: 'loginCtrl'
+        controller: 'loginCtrl',
+        controllerAs: 'vm'
       })
 
-        .state('app', {
-        url: '/app',
-        abstract: true,
-        templateUrl: 'templates/menu.html',
-        controller: 'AppCtrl'
-      })
-
-      .state('app.search', {
-        url: '/search',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/search.html'
-          }
-        }
-      })
-
-      .state('app.browse', {
-          url: '/browse',
-          views: {
-            'menuContent': {
-              templateUrl: 'templates/browse.html'
-            }
-          }
-        })
-        .state('app.playlists', {
-          url: '/playlists',
-          views: {
-            'menuContent': {
-              templateUrl: 'templates/playlists.html',
-              controller: 'PlaylistsCtrl'
-            }
-          }
-        })
-
-      .state('app.single', {
-        url: '/playlists/:playlistId',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/playlist.html',
-            controller: 'PlaylistCtrl'
-          }
-        }
+      .state('task', {
+        url: '/task',  
+        templateUrl: 'templates/task.html',
+        controller: 'taskCtrl',
+        controllerAs: 'vm'
       });
       // if none of the above states are matched, use this as the fallback
       $urlRouterProvider.otherwise('/login');
