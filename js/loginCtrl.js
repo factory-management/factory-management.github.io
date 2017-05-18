8ff44114577c845d97b062f88f96169bff31435a
AshokNaik <ashonaik@in.ibm.com>
2017-05-18 12:47:24 +0530

    login controller uploaded

--- js/loginCtrl.js
+++ js/loginCtrl.js
@@ -3,45 +3,59 @@
 
       'use strict';
     
-myApp.controller('loginCtrl', function($scope) {
-      $scope.signInScreen = true;
-      $scope.pswdblankValidation = false;
-      $scope.emailblankValidation=false;
-
-    $scope.signInSubmit = function(){
-      $scope.emailblankValidation=true;
-      $scope.pswdblankValidation = true;
-    }
-    $scope.signUpSubmit = function(){
-      $scope.signUpemailblankValidation=true;
-      $scope.signUpPswdblankValidation = true;
-    }
-    $scope.resetPswd = function(){
-      $scope.resetMailblankValidation=true;
-    }
-    $scope.signUp = function(){
-      $scope.forgotPswdScreen = false;
-      $scope.signInScreen = false;
-      $scope.signUpScreen = true; 
-           /*alert($scope.signUpScreen);*/
-    }
-    $scope.signIn = function(){
-      $scope.signUpScreen = false;
-      $scope.signInScreen = true;
-    }
-    $scope.forgotPswd = function(){
-      $scope.signInScreen = false;
-      $scope.forgotPswdScreen = true;
-      /*alert($scope.signUpScreen);*/
-    }
-
-     var myDate = new Date();
-        var hrs = myDate.getHours();
-        if (hrs < 12)
-             $scope.greet = 'Good Morning';
+myApp.controller('loginCtrl', function($scope, $state) {
+    var vm = this;
+    vm.signInScreen = true;
+    
+    var myDate = new Date();
+    var hrs = myDate.getHours();
+      if (hrs < 12)
+          $scope.greet = 'Good Morning';
         else if (hrs >= 12 && hrs <= 17)
-             $scope.greet = 'Good Afternoon';
+            $scope.greet = 'Good Afternoon';
         else if (hrs >= 17 && hrs <= 24)
-             $scope.greet = 'Good Evening';
-    })
+            $scope.greet = 'Good Evening';
+
+    vm.signInSubmit = function(){   
+      if((vm.signIn.email =='') || (vm.signIn.email ==undefined) || (vm.signIn.password =='') || (vm.signIn.password ==undefined)){
+              $scope.emailblankValidation=true;
+              $scope.pswdblankValidation = true;
+          }else{
+              $state.go('task')
+        }
+    };
+    vm.signUpSubmit = function(){
+      if((vm.signUp.email =='') || (vm.signUp.email ==undefined) || (vm.signUp.password =='') || (vm.signUp.password ==undefined)){
+              $scope.signUpemailblankValidation=true;
+              $scope.signUpPswdblankValidation=true;
+          }else{
+              vm.signUpScreen = false;
+              vm.successSignUpScreen =true;
+        }
+    };
+    vm.resetPswd = function(){
+      $scope.resetMailblankValidation=true;
+      if((vm.reset.email =='') || (vm.reset.email ==undefined)){
+              $scope.resetMailblankValidation=true;
+          }else{
+              vm.forgotPswdScreen=false;
+              vm.pswdResetScreen=true;
+        }  
+    }
+
+    vm.signUp = function(){
+      vm.forgotPswdScreen = false;
+      vm.signInScreen = false;
+      vm.pswdResetScreen = false;
+      vm.signUpScreen = true;
+    }
+    vm.signIn = function(){
+      vm.signUpScreen = false;
+      vm.signInScreen = true;
+    }
+    vm.forgotPswd = function(){
+      vm.signInScreen = false;
+      vm.forgotPswdScreen = true;
+    }      
+  })
 }());
